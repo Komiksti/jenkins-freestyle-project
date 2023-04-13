cb9d64f62e5a92c71c80999d675f70e9ea949b39
Komiksti <101198272+Komiksti@users.noreply.github.com>
2023-04-13 14:19:07 +0100

    Add files via upload
    Fix for zip bug

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 echo "'Hello from the Jenkins job named: ${JOB_NAME}"
+docker exec --user root -it Jenkins /bin/bash then apt-get -y update then apt-get install zip -y
 touch 1.txt 2.txt 3.txt 4.txt 5.txt
 echo "Some info1" > 1.txt
 echo "Some info2" > 2.txt
