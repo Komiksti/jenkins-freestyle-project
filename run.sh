59dd7340cf5343c44f65e980e5de5184500dbb7c
Komiksti <101198272+Komiksti@users.noreply.github.com>
2023-04-13 14:12:50 +0100

    Add files via upload
    3rd test

--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!bin/bash
 echo "'Hello from the Jenkins job named: ${JOB_NAME}"
+echo "Some info1" > 1.txt
+echo "Some info2" > 2.txt
+echo "Some info3" > 3.txt
+echo "Some info4" > 4.txt
+echo "Some info5" > 5.txt
+zip archive.zip *.txt
